7dd0dd8a96f22566d0520f408c4afa73b0ab0b19
Sarbaj <sarbajmalek3456@gmail.com>
2025-06-19 14:00:22 +0530

    undo

---
+++ BACKEND/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import DbConnection from './db/Db.js';
+import router from './routes/Router.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+  origin: ["https://vivaportal.vercel.app"],
+  credentials: true,
+}));
+app.use(express.json());
+
+DbConnection(); // MongoDB connect
+
+app.get('/', (req, res) => res.send("Server running"));
+
+app.use('/bin', router);
+
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
