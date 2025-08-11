c058ba96b5cee6e03f934ff3de76ad843258a85d
Sarbaj <sarbajmalek3456@gmail.com>
2025-08-11 23:21:34 +0530

    Backend Localhost path

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,23 +1,25 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import DbConnection from './db/Db.js';
-import router from './routes/Router.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import DbConnection from "./db/Db.js";
+import router from "./routes/Router.js";
 
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: ["https://vivaportal.vercel.app"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["https://vivaportal.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 DbConnection(); // MongoDB connect
 
-app.get('/', (req, res) => res.send("Server running"));
+app.get("/", (req, res) => res.send("Server running"));
 
-app.use('/bin', router);
+app.use("/bin", router);
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
