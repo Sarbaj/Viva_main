4f1142e91fc554eb9d268a481a0d24c8ab649c29
Sarbaj <sarbajmalek3456@gmail.com>
2025-08-12 14:24:04 +0530

    localhost

--- BACKEND/db/Db.js
+++ BACKEND/db/Db.js
@@ -1,18 +1,15 @@
 import { connect } from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-const DbConnection=async()=>{
-
-    try {
-        const Conn=await connect(process.env.DATABASEURI)
-        if (Conn) {
-            console.log(`✅ Database Connected: ${Conn.connection.host}`);
-        }
-        
-    } catch (error) {   
-        console.error('error on connection');
+const DbConnection = async () => {
+  try {
+    const Conn = await connect(process.env.DATABASEURI);
+    if (Conn) {
+      console.log(`✅ Database Connected: ${Conn.connection.host}`);
     }
-
-}
+  } catch (error) {
+    console.error("error on connection");
+  }
+};
 export default DbConnection;
