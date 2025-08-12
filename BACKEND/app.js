952396de322f05c72e09c56d3472d4b26eabe75f
Sarbaj <sarbajmalek3456@gmail.com>
2025-08-12 14:19:32 +0530

    push localhost link

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://vivaportal.vercel.app"],
+    origin: ["https://vivaportal.vercel.app", "http://localhost:5173/"],
     credentials: true,
   })
 );
